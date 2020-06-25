e5f49c3d0025515c8220df39be4303bcfa48126b
Akash bisariya <akash.bisariya@nagarro.com>
2020-06-25 16:02:40 +0530

    Completed excercise 1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,54 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+    private NegativeNumberValidator negativeNumberValidator;
+
+    @Before
+    public void setUp(){
+        negativeNumberValidator = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1(){
+        boolean res = negativeNumberValidator.isNegative(-1);
+        Assert.assertThat(res,is(true));
+    }
+
+    @Test
+    public void test2(){
+        boolean res = negativeNumberValidator.isNegative(1);
+        Assert.assertThat(res,is(false));
+    }
+
+    @Test
+    public void test3(){
+        boolean res = negativeNumberValidator.isNegative(0);
+        Assert.assertThat(res,is(false));
+    }
+
+    @Test
+    public void test4(){
+        boolean res = negativeNumberValidator.isNegative(-5);
+        Assert.assertThat(res,is(true));
+    }
+
+    @Test
+    public void test5(){
+        boolean res = negativeNumberValidator.isNegative(-100);
+        Assert.assertThat(res,is(true));
+    }
+
+    @Test
+    public void test6(){
+        boolean res = negativeNumberValidator.isNegative(100);
+        Assert.assertThat(res,is(false));
+    }
+
 
 }
