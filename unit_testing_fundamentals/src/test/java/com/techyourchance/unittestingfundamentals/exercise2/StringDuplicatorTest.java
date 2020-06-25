60ec7403b0b5e2d54a651fcc5a5f21df8b0d43a6
Akash bisariya <akash.bisariya@nagarro.com>
2020-06-25 16:03:49 +0530

    Completed excercise 2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,42 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class StringDuplicatorTest {
+    StringDuplicator stringDuplicator;
+
+    @Before
+    public void setUp() throws Exception {
+        stringDuplicator = new StringDuplicator();
+    }
+
+    @Test
+    public void test1() {
+        String val = stringDuplicator.duplicate("akash");
+        Assert.assertThat(val, is("akashakash"));
+    }
+
+    @Test
+    public void test2() {
+        String val  = stringDuplicator.duplicate("1");
+        Assert.assertThat(val,is("11"));
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String val  = stringDuplicator.duplicate("");
+        Assert.assertThat(val,is(""));
+    }
+
+    @Test
+    public void duplicate_longString_duplicatedStringReturned() {
+        String val  = stringDuplicator.duplicate("Akash shankar Bisariya");
+        Assert.assertThat(val,is("Akash shankar BisariyaAkash shankar Bisariya"));
+    }
+
 
 }
